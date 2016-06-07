da3ab0a9ddda461e70dc317849a486fea263b6f2
Paul Murray <pmurray@bigpond.com>
2016-06-07 14:59:44 +1000

    Ok. This works nicely.

--- arduino_dougsauto_tacho.ino
+++ arduino_dougsauto_tacho.ino
@@ -8,7 +8,10 @@ class MyStepper {
     const unsigned long maxSpeedUs;
 
 
-    MyStepper( const byte ApinAttach, const byte  _ApinAttach,  const byte BpinAttach,  const byte _BpinAttach, const unsigned long maxSpeedUs) :
+    MyStepper(
+      const byte ApinAttach, const byte  _ApinAttach,
+      const byte BpinAttach,  const byte _BpinAttach,
+      const unsigned long maxSpeedUs) :
       Apin(ApinAttach), _Apin(_ApinAttach), Bpin(BpinAttach), _Bpin(_BpinAttach), maxSpeedUs(maxSpeedUs)
     {}
 
@@ -68,8 +71,12 @@ class MyStepper {
       }
     }
 
+    inline int getTarget() {
+      return target - zero;
+    }
+
     void moveTo(int _target) {
-     target = _target + zero;
+      target = _target + zero;
     }
 
     void zeroHere() {
@@ -83,19 +90,56 @@ class MyStepper {
 };
 
 
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+    OK. This section includes a number of constants that will help you smooth
+    the motion of the needle. As alsways, here's a trade off: the smoother you make the needle, \
+    the less responsive it will be
+*/
+
+
+/*  this constant governs how fast the needle moves. For my stepper, it's pretty slow. Your
+    instrument stepper should be much faster and use a much smaler value. If you make it too small,
+    the stepper will lose track of where zero is - it's registration will slip.
+*/
+
+const unsigned long NEEDLE_MAX_RATE_us = 6000;
+
+/**
+   This constant coverns the low pass filter on the sampling. A value of 1 means no filtering.
+   Higer values than this mean that the time between pulses gets averaged out, as a cost of the
+   value taking time to reach the real value
+*/
+
+const int SAMPLE_LOW_PASS_FILTER = 4;
+
+/**
+   This value means that the sketch will not attempt to move the needle unless it has moved
+   at least this far into the range of the next needle position segment.
+*/
+
+const double NEEDLE_HYSTERESIS = .5;
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This section inclues various bounds and settings that together determine 
+ * how the needle travels.
+ */
+
 /**
    I have a 48-pole stepper, and I want the dial
    to cover 3/4rds of the circle, so I am setting the steps to 48*3/4 = 36
 */
 
-const int STEPS = 36;
+const int STEPS = 48 * 3 / 4;
 
-// this is the frequency in Hz at which the steeper should be set to STEPS
+// this is the frequency in Hz at which the steepper should be set to STEPS
+// note that 
 const double MAX_FREQ = 2050;
 
-// my stepper motor is pretty slow, so I will use 5 millis as the maximum step speed
-// the instrument stepper can almost certainly run quite a bit quicker
-MyStepper tacho(3, 5, 6, 4, 10000);
+MyStepper tacho(3, 5, 6, 4, NEEDLE_MAX_RATE_us);
 
 // this is the gear to count the width of the pulses
 
@@ -105,18 +149,17 @@ long lastPulseUs;
 
 void pulseISR() {
   unsigned long pulseUs = micros();
-  // this does a rolling average of the pulse length. 
-  pulseLengthUs = (pulseLengthUs * 3 + (pulseUs - lastPulseUs)) / 4;
+  // this does a rolling average of the pulse length.
+  pulseLengthUs = (pulseLengthUs * (SAMPLE_LOW_PASS_FILTER - 1) + (pulseUs - lastPulseUs)) / SAMPLE_LOW_PASS_FILTER;
   lastPulseUs = pulseUs;
 }
 
 void setup() {
-  Serial.begin(57600);
-  while (!Serial);
-
   tacho.setup();
 
-  // to zero the tacho, we move forward STEPS * 1.25 the stop and then back by STEPS exactly;
+  // to zero the tacho, we move forward STEPS * 1.25 the stop and then back by STEPS exactly
+  // this will zero it against the end stop at the limit of travel.
+  
   tacho.moveTo(STEPS * 5 / 4);
   while (tacho.isMoving()) tacho.loop();
   tacho.zeroHere();
@@ -125,36 +168,39 @@ void setup() {
   while (tacho.isMoving()) tacho.loop();
   tacho.zeroHere();
 
-  // and I move the needle between 0 and the limit just so I can see it for myself.
+  // and I move the needle between 0 and the limit just so I can see it move.
+  tacho.moveTo(STEPS);
+  while (tacho.isMoving()) tacho.loop();
+  tacho.moveTo(0);
+  while (tacho.isMoving()) tacho.loop();
 
-  for (int i = 0; i < 3; i++) {
-    tacho.moveTo(STEPS);
-    while (tacho.isMoving()) tacho.loop();
-    tacho.moveTo(0);
-    while (tacho.isMoving()) tacho.loop();
-  }
-
+  // ok: hook up the tacho sensor.
   attachInterrupt(digitalPinToInterrupt(2), pulseISR, RISING);
-
 }
 
 void loop() {
   tacho.loop();
 
+  // make this as fast as possible to reduce glitching
   noInterrupts();
   unsigned long pulseCpy = pulseLengthUs;
   interrupts();
 
-  int newTarget;
-  if(pulseLengthUs == 0)
-  newTarget = 0;
+  double newTarget;
+
+  if (pulseLengthUs == 0)
+    newTarget = 0;
   else {
     newTarget = 1000000.0 / (double)pulseCpy / MAX_FREQ * STEPS;
   }
 
-  if(newTarget > STEPS) newTarget = STEPS;
+  if (newTarget < 0) newTarget = 0;
+  else if (newTarget > STEPS) newTarget = STEPS;
 
-  tacho.moveTo(newTarget);
+  // hysteresis
+  if (newTarget < tacho.getTarget() - NEEDLE_HYSTERESIS || newTarget > tacho.getTarget() + 1 + NEEDLE_HYSTERESIS) {
+    tacho.moveTo(newTarget);
+  }
 }
 
 
