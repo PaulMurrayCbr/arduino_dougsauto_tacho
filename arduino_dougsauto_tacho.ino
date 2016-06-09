8f44fd47f6e61da1892fc4487f7b48118d0474e5
Paul Murray <pmurray@bigpond.com>
2016-06-09 20:05:37 +1000

    added zero stop

--- arduino_dougsauto_tacho.ino
+++ arduino_dougsauto_tacho.ino
@@ -109,37 +109,47 @@ class MyStepper {
 
 class Sampler {
     // make this static because otherwise i have to malloc things, which is a pain
-    static const int RING_BUFFER_SIZE = 4;
+    static const int RING_BUFFER_SIZE = 50;
     const float LOWPASS_DAMPING;
     const float MAX_FREQ;
     const int STEPS;
     const float NEEDLE_HYSTERESIS;
 
+    // a signal coming in at this rate will have a frequency
+    // definitely below MIN_FREQ.
+    const unsigned long ZERO_SPEED_us;
+
     MyStepper &stepper;
 
+    volatile long mostRecentPulseUs;
     volatile long ringBufferUs[RING_BUFFER_SIZE];
     volatile int ringBufferPos = 0;
     volatile float avgPulseWidthUs;
 
   public:
 
-    Sampler(const float LOWPASS_DAMPING, const float MAX_FREQ,  const int STEPS, const float NEEDLE_HYSTERESIS, MyStepper &stepper ) :
+    Sampler(const float LOWPASS_DAMPING, const float MAX_FREQ,  const float MIN_FREQ, const int STEPS, const float NEEDLE_HYSTERESIS, MyStepper &stepper ) :
       LOWPASS_DAMPING(LOWPASS_DAMPING) ,
       MAX_FREQ(MAX_FREQ),
       STEPS(STEPS),
       NEEDLE_HYSTERESIS(NEEDLE_HYSTERESIS),
-      stepper(stepper)
-    {}
+      stepper(stepper),
+      ZERO_SPEED_us(1000000 / MIN_FREQ)
+    {
+    }
 
     void pulseISR() {
-      unsigned long us = micros();
-      unsigned long pulseWidthUs = us - ringBufferUs[ringBufferPos];
-      ringBufferUs[ringBufferPos] = us;
       if (++ringBufferPos >= RING_BUFFER_SIZE) {
         ringBufferPos = 0;
       }
+      
+      mostRecentPulseUs = micros();
+      unsigned long pulseWidthUs = mostRecentPulseUs - ringBufferUs[ringBufferPos];
+      ringBufferUs[ringBufferPos] = mostRecentPulseUs;
 
-      avgPulseWidthUs = LOWPASS_DAMPING * avgPulseWidthUs + (1 - LOWPASS_DAMPING) * pulseWidthUs / RING_BUFFER_SIZE;
+      // these floating point operations may cause problems on account of being slow and hapenning inside an ISR
+
+      avgPulseWidthUs = LOWPASS_DAMPING * avgPulseWidthUs + (1 - LOWPASS_DAMPING) * (float)pulseWidthUs / RING_BUFFER_SIZE;
     }
 
     void setup() {
@@ -151,25 +161,35 @@ class Sampler {
       while (stepper.isMoving()) stepper.loop();
       stepper.zeroHere();
 
+
       // and I move the needle between 0 and the limit just so I can see it move.
-      stepper.moveTo(STEPS);
-      while (stepper.isMoving()) stepper.loop();
-      stepper.moveTo(0);
-      while (stepper.isMoving()) stepper.loop();
+      //      stepper.moveTo(STEPS);
+      //      while (stepper.isMoving()) stepper.loop();
+      //      stepper.moveTo(0);
+      //      while (stepper.isMoving()) stepper.loop();
     }
 
     void loop() {
       // make this as fast as possible to reduce glitching
       noInterrupts();
-      unsigned long avgPulseWidthCpyUs = avgPulseWidthUs;
-      interrupts();
+      
+      // if the mst recent pulse was a long time ago (as specified by ZERO_SPEED_us),
+      // then we manually zero the speed
+      if (micros() - mostRecentPulseUs > ZERO_SPEED_us && avgPulseWidthUs <= ZERO_SPEED_us) {
+        avgPulseWidthUs = ZERO_SPEED_us * 2.0; // times two to give us plenty of slop
+      }
 
+      // this needs to be in a nointerrupts block because 
+      // flat read and writes are not necessarily atomic
+      float avgPulseWidthCpyUs = avgPulseWidthUs;
+      interrupts();
+      
       double newTarget;
 
-      if (avgPulseWidthCpyUs == 0)
+      if (avgPulseWidthCpyUs == 0 || avgPulseWidthCpyUs >= ZERO_SPEED_us)
         newTarget = 0;
       else {
-        newTarget = 1000000.0 / (double)avgPulseWidthCpyUs / MAX_FREQ * STEPS;
+        newTarget = 1000000.0 / avgPulseWidthCpyUs / MAX_FREQ * STEPS;
       }
 
       if (newTarget < 0) newTarget = 0;
@@ -183,38 +203,34 @@ class Sampler {
 
 };
 
-MyStepper tacho(3, 5, 6, 4, 6000); // pins 4,5,6,4, max rate 6000us because I have flattened my batteries :(
+MyStepper tacho(5, 7, 8, 6, 3000); // pins 4,5,6,4, max rate 6000us because I have flattened my batteries :(
+MyStepper speedo(9, 11, 12, 10, 3000); // pins 4,5,6,4, max rate 6000us because I have flattened my batteries :(
 
 Sampler tachoSampler(
   .75, // low-pass damping. 0 means none, 1 means no signal at all
   2050,   // max frequency
+  5,   // min frequency
   48 * 2 * 3 / 4, // number of steps on the stepper
   .6, // hysteresis at .5, there is a microscopic chance of needle vibration, so set it to just over that
   tacho // the stepper motor to which this instance is bound
 );
 
-#ifdef YES_WE_HAVE_A_SECOND_GAUGE // comment this next section out
-// to define another gauge and senesor:
-MyStepper secondTacho(12, 11, 10, 9, 6000); // pins 4,5,6,4, max rate 6000us because I have flattened my batteries :(
-
-Sampler secondTachoSampler(
-  .75, // low-pass compression. 0 means none, 1 means no signal at all
+Sampler speedoSampler(
+  .75, // low-pass damping. 0 means none, 1 means no signal at all
   2050,   // max frequency
-  48 * 2 * 3 / 4, // number of steps on the meter
+   512,   // min frequency
+  48 * 2 * 3 / 4, // number of steps on the stepper
   .6, // hysteresis at .5, there is a microscopic chance of needle vibration, so set it to just over that
-  secondTacho // the stepper motor to which this instance is bound
+  speedo // the stepper motor to which this instance is bound
 );
-#endif
 
 void pin2ISR() {
   tachoSampler.pulseISR();
 }
 
-#ifdef YES_WE_HAVE_A_SECOND_GAUGE // comment this next section out
 void pin3ISR() {
-  secondTachoSampler.pulseISR();
+  speedoSampler.pulseISR();
 }
-#endif
 
 void setup() {
   tacho.setup();
@@ -222,25 +238,17 @@ void setup() {
   pinMode(2, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), pin2ISR, RISING);
 
-#ifdef YES_WE_HAVE_A_SECOND_GAUGE // comment this next section out
-  secondTacho.setup();
-  secondTachoSampler.setup(); // the stepper must be set up before the sampler that uses it
-  // *******
-  // OBVIOUSLY THAT FIRST STEPPR NEEDS TO BE MOVED TO A DIFFERENT SET OF PINS FOR THIS TO WORK
-  // *******
+  speedo.setup();
+  speedoSampler.setup(); // the stepper must be set up before the sampler that uses it
   pinMode(3, INPUT);
   attachInterrupt(digitalPinToInterrupt(3), pin3ISR, RISING);
-#endif
 }
 
 void loop() {
   tacho.loop();
   tachoSampler.loop();
-
-#ifdef YES_WE_HAVE_A_SECOND_GAUGE // comment this next section out
-  secondTacho.loop();
-  secondTachoSampler.loop();
-#endif
+  speedo.loop();
+  speedoSampler.loop();
 }
 
 
